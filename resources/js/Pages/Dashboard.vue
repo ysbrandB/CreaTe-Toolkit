a1aed417d188049b91123d667414e6ddcdc2f27b
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-05-27 12:40:58 +0200

    make dashboard more different

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import {Head} from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Dashboard"/>
 
     <AuthenticatedLayout>
         <template #header>
@@ -19,10 +19,15 @@ import { Head } from '@inertiajs/vue3';
             </div>
         </div>
 
-        <div class="pt-12 w-50">
+        <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Choice helper</div>
+                <div class="flex flex-row justify-center items-center">
+                    <div class="w-full p-8 mr-3 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 text-2xl dark:text-gray-100">Actuating</div>
+                    </div>
+                    <div class="ml-3 w-full p-8 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 text-2xl dark:text-gray-100">Sensing</div>
+                    </div>
                 </div>
             </div>
         </div>
