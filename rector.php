85c4329632445307c3d8537cdd5c5c2ecab2b612
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-06-16 00:02:24 +0200

    Try the other way of giving .env to the action

--- rector.php
+++ rector.php
@@ -7,7 +7,6 @@ use Rector\Config\RectorConfig;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
         __DIR__.'/config',
         __DIR__.'/resources',
         __DIR__.'/routes',
