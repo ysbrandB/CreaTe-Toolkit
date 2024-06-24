9b67a7f040c897a19be7872f24ae2bc935acfa72
ysbrandB <ysbrandB@users.noreply.github.com>
2024-06-24 09:35:55 +0000

    Fix code style

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Sentry\Laravel\Integration;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
